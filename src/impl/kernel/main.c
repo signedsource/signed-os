b249b389f81f62a3fc8625d8eec67590633419ff
Sherikxd <bartolo12322@gmail.com>
2021-12-27 22:12:06 -0500

    Update main.c

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -4,11 +4,11 @@
 void kernel_main() {
     print_clear();
     print_set_color(PRINT_COLOR_WHITE, PRINT_COLOR_BLACK);
-    printf("***\n");
+    printf("****************************************************************\n");
     printf("Welcome to ");
     print_set_color(PRINT_COLOR_LIGHT_RED, PRINT_COLOR_BLACK);
-    printf("SignedOS\n");
+    printf("SignedOS  ^_^ \n");
     print_set_color(PRINT_COLOR_WHITE, PRINT_COLOR_BLACK);
-    printf("***");
+    printf("****************************************************************");
     dos();
 }
